7679d3169c8184d8d8ad1fcf979286322dfade8e
Botond Dáni <dani.botond@nordlogic.com>
2013-08-15 22:54:09 +0300

    Resolved conflicts

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 name             'geonames_importer'
-=======
-name             'geonames-importer'
->>>>>>> 2227bae5ebe2657802d008c403f19dcd80afc8b8
 maintainer       'Botond Dani'
 maintainer_email 'hi@danibotond.ro'
 license          'MIT'
@@ -11,10 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
 depends 		 'mysql'
-<<<<<<< HEAD
 
 %w(ubuntu debian).each do |os|
 	supports os
 end
-=======
->>>>>>> 2227bae5ebe2657802d008c403f19dcd80afc8b8
